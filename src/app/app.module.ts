666c126b8685749c03bef294cc5bfe8aebb891a2
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-09-12 23:28:00 +0530

    project initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatButtonToggleModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
